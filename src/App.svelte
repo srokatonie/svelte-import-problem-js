d472e2e805d24ca5e7610d66b9fd69fc48ccdd23
Maciej Sroka <srokatonie@gmail.com>
2021-11-02 08:25:06 +0100

    Init commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { onMount } from 'svelte';
+	import moment from 'moment'
+
+	// import web3 from 'web3'
+
+	onMount(() => {
+
+    // Working fine
+		console.log(moment().format('dddd'))
+
+	})
+
+</script>
+
+<main>
+	<h1>Hello!</h1>
+</main>
